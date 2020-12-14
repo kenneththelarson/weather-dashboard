ec0a08328d8689c1369e207445158bdd72313fb7
Kenny <kenneththelarson@gmail.com>
2020-12-13 20:39:42 -0700

    title and date working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var apiKey = "91e715b2f0f8a386b9670f18abfb2040";
 var cityFormEl = document.querySelector("#city-search");
 var cityInputEl = document.querySelector("#city-name");
+var currentWeatherEl = document.querySelector("#current-weather-container");
+var currentCityTitleEL = document.querySelector("#current-city");
+var cityTitleEl = document.querySelector("#city-title");
 
 var searchCityInput = function (event) {
     event.preventDefault();
@@ -9,7 +12,6 @@ var searchCityInput = function (event) {
     if (currentCity) {
         getCityWeather(currentCity)
         cityInputEl.value = "";
-        console.log(currentCity);
     }
     else {
         alert("Please enter a city name in the search bar!");
@@ -20,8 +22,27 @@ var getCityWeather = function (city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
     fetch(apiUrl).then(function (response) {
-        console.log(response, city);
+        if (response.ok) {
+            response.json().then(function(data) {
+                loadWeather(data, city);
+            })
+        }
+        else {
+            alert("Error: " + response.statusText);
+            return;
+        }
     });
+
 };
+
+var loadWeather = function(weather, currentCity) {
+    currentWeatherEl.textContent = "";
+    cityTitleEl.textContent = currentCity;
+    currentWeatherEl.classList.add("border");
+
+    var currentDate = document.createElement("span");
+    currentDate.textContent = " (" + moment().format('M/DD/YYYY') + ")";
+    cityTitleEl.appendChild(currentDate);
+}
 
 cityFormEl.addEventListener("submit", searchCityInput);
