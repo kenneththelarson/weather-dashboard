34236a5838860dcaad891ea4c1ba9050833283fc
Kenny <kenneththelarson@gmail.com>
2020-12-13 21:42:10 -0700

    display temp humidity wind and uv

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ var getCityWeather = function (city) {
 
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
-            response.json().then(function(data) {
+            response.json().then(function (data) {
                 loadWeather(data, city);
             })
         }
@@ -35,14 +35,71 @@ var getCityWeather = function (city) {
 
 };
 
-var loadWeather = function(weather, currentCity) {
+var loadWeather = function (weather, currentCity) {
     currentWeatherEl.textContent = "";
     cityTitleEl.textContent = currentCity;
-    currentWeatherEl.classList.add("border");
+    currentWeatherEl.classList.add("border", "p-2");
 
     var currentDate = document.createElement("span");
     currentDate.textContent = " (" + moment().format('M/DD/YYYY') + ")";
     cityTitleEl.appendChild(currentDate);
+
+    var weatherIcon = document.createElement('img');
+    weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    cityTitleEl.appendChild(weatherIcon);
+
+    var tempEl = document.createElement("span");
+    tempEl.textContent = "Temperature: " + weather.main.temp + " °F";
+    tempEl.classList = "list-group-item";
+    currentWeatherEl.appendChild(tempEl);
+
+    var humidityEl = document.createElement("span");
+    humidityEl.textContent = "Humidity: " + weather.main.humidity + "%";
+    humidityEl.classList = "list-group-item";
+    currentWeatherEl.appendChild(humidityEl);
+
+    var windEl = document.createElement("span");
+    windEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+    windEl.classList = "list-group-item";
+    currentWeatherEl.appendChild(windEl);
+
+    var latitude = weather.coord.lat;
+    var longitude = weather.coord.lon;
+
+    getUvIndex(latitude, longitude);
+};
+
+var getUvIndex = function (latitude, longitude) {
+    var apiUV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+
+    fetch(apiUV).then(function (response) {
+        response.json().then(function (data) {
+            displayUV(data);
+            console.log(data);
+        });
+    });
+};
+
+var displayUV = function(data) {
+    var uvEl = document.createElement("div");
+    uvEl.textContent = "UV Index: ";
+    uvEl.classList = "list-group-item";
+
+    var uvCondition = document.createElement("span");
+    uvCondition.textContent = data.value;
+
+    if (data.value <= 2) {
+        uvCondition.classList = "favorable";
+    } 
+    else if (data.value > 2 && data.value <= 8) {
+        uvCondition.classList = "moderate";
+    }
+    else if (data.value > 8) {
+        uvCondition.classList = "severe";
+    };
+
+    uvEl.appendChild(uvCondition);
+    currentWeatherEl.appendChild(uvEl);
 }
 
 cityFormEl.addEventListener("submit", searchCityInput);
